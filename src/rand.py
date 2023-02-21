1e75e20be7bb35cd40b74404a8b147c98cc0d360
Tanushree Bhattacharya <55945733+tanu272@users.noreply.github.com>
2023-02-21 14:57:51 +0530

    Add files via upload

---
+++ src/rand.py
@@ -0,0 +1,290 @@
+import re # regex
+import pandas as pd # tables
+import matplotlib.pyplot as plt # plots
+import seaborn as sns # plots
+import numpy as np 
+
+test = pd.read_csv("./test.txt", header=None, sep=";", names=["Lines","Emotions"], encoding="utf-8")
+train = pd.read_csv("./train.txt", header=None, sep=";", names=["Lines","Emotions"], encoding="utf-8")
+val = pd.read_csv("./val.txt", header=None, sep=";", names=["Lines","Emotions"], encoding="utf-8")
+
+emotions_to_labels = {'anger': 0, 'love': 1, 'fear': 2, 'joy': 3, 'sadness': 4,'surprise': 5}
+labels_to_emotions = {j:i for i,j in emotions_to_labels.items()}
+
+train['Labels'] = train['Emotions'].replace(emotions_to_labels)
+test['Labels'] = test['Emotions'].replace(emotions_to_labels)
+val['Labels'] = val['Emotions'].replace(emotions_to_labels)
+
+print(train.head())
+
+
+## visualize the data
+""" def visualize_labels_distribution(df, title='the'):
+  '''
+  Accepts a dataframe with 'Emotions' column and dataset title (e.g. 'train')
+  Creates bar chart with num of elements of each category
+  Returns nothing
+
+  '''
+  # create a pandas series with labels and their counts
+  num_labels = df['Emotions'].value_counts()
+
+  # num of unique categories
+  x_barchart = range(df['Emotions'].nunique())
+  # list of labels
+  x_barchart_labels = [str(emotions_to_labels[emotion]) +\
+                       ' - ' + emotion for emotion in list(num_labels.index)]
+
+  # list of counts
+  y_barchart = list(num_labels.values)
+
+  # creating bar chart
+  plt.figure(figsize = (5, 4))
+  plt.bar(x_barchart, y_barchart, color='#707bfb')
+  # adding num of elements for each category on plot as text
+  for index, data in enumerate(y_barchart):
+    plt.text(x = index, 
+            y = data+max(y_barchart)/100, 
+            s = '{}'.format(data), 
+            fontdict = dict(fontsize=10), 
+            ha = 'center',)
+  
+  plt.xticks(x_barchart, x_barchart_labels, rotation=40)
+  plt.title('Num of elements of each category for {} dataset'.format(title))
+  plt.tight_layout()
+
+  print('There are {} records in the dataset.\n'.format(len(df.index)))
+
+  plt.show()
+
+visualize_labels_distribution(train, 'train')
+visualize_labels_distribution(test, 'test')
+visualize_labels_distribution(val, 'val') 
+ """
+
+import nltk 
+from nltk.corpus import stopwords
+
+# downloading a set of stop-words
+STOPWORDS = set(stopwords.words('english'))
+
+# tokenizer
+from nltk.tokenize import word_tokenize
+
+def text_preprocess(text, stop_words=False):
+  '''
+  Accepts text (a single string) and
+  a parameters of preprocessing
+  Returns preprocessed text
+
+  '''
+  # clean text from non-words
+  text = re.sub(r'\W+', ' ', text).lower()
+
+  # tokenize the text
+  tokens = word_tokenize(text)
+
+  if stop_words:
+    # delete stop_words
+    tokens = [token for token in tokens if token not in STOPWORDS]
+
+  return tokens
+
+print('Before: ')
+print(train.head())
+
+x_train = [text_preprocess(t, stop_words=True) for t in train['Lines']]
+y_train = train['Labels'].values
+
+print('\nAfter:')
+for line_and_label in list(zip(x_train[:5], y_train[:5])):
+  print(line_and_label)
+
+x_test = [text_preprocess(t, stop_words=True) for t in test['Lines']]
+y_test = test['Labels'].values
+
+x_validation = [text_preprocess(t, stop_words=True) for t in val['Lines']]
+y_validation = val['Labels'].values
+
+import gensim.downloader as api
+model_wiki = api.load('fasttext-wiki-news-subwords-300')
+
+from gensim.models import Word2Vec
+
+# train word2vec model on the corpus
+model_w2v = Word2Vec(x_train + x_test + x_validation, vector_size = 300, min_count = 2).wv  
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+DICT_SIZE = 15000
+
+# creating a dictionary with most used words
+# where num of words = DICT_SIZE
+tokenizer = Tokenizer(num_words=DICT_SIZE)
+total = x_train + x_train + x_validation
+tokenizer.fit_on_texts(total)
+
+print(list(tokenizer.word_index.items())[:5])
+print(len(tokenizer.word_index))
+
+x_train_max_len = max([len(i) for i in x_train])
+x_test_max_len = max([len(i) for i in x_test])
+x_validation_max_len = max([len(i) for i in x_validation])
+
+MAX_LEN = max(x_train_max_len, x_test_max_len, x_validation_max_len)
+print(MAX_LEN)
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+X_train = tokenizer.texts_to_sequences(x_train)
+X_train_pad = pad_sequences(X_train, maxlen=MAX_LEN)
+
+X_test = tokenizer.texts_to_sequences(x_test)
+X_test_pad = pad_sequences(X_test, maxlen=MAX_LEN)
+
+X_val = tokenizer.texts_to_sequences(x_validation)
+X_val_pad = pad_sequences(X_val, maxlen=MAX_LEN)
+
+print(X_train[0])
+
+print(X_train_pad[0])
+
+def create_weight_matrix(model, second_model=False):
+  '''
+  Accepts word embedding model
+  and the second model, if provided
+  Returns weight matrix of size m*n, where
+  m - size of the dictionary
+  n - size of the word embedding vector
+
+  '''
+  vector_size = model.get_vector('like').shape[0]
+  w_matrix = np.zeros((DICT_SIZE, vector_size))
+  skipped_words = []
+
+  for word, index in tokenizer.word_index.items():
+    if index < DICT_SIZE:
+      if word in model.key_to_index: 
+        w_matrix[index] = model.get_vector(word)
+      else:
+        if second_model:
+          if word in second_model.key_to_index:
+            w_matrix[index] = second_model.get_vector(word)
+          else:
+            skipped_words.append(word)
+        else:
+          skipped_words.append(word)
+ 
+  print(f'{len(skipped_words)} wors were skipped. Some of them:')
+  print(skipped_words[:50])
+  return w_matrix
+
+weight_matrix = create_weight_matrix(model_wiki, model_w2v)
+print(weight_matrix.shape)
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Bidirectional, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+stop = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=3)
+
+model = Sequential()
+model.add(Embedding(input_dim = DICT_SIZE, # the whole vocabulary size
+                    output_dim = weight_matrix.shape[1], # vector space dimension
+                    input_length = X_train_pad.shape[1], # max_len of text sequence
+                    weights=[weight_matrix], # assign the embedding weight with embedding marix
+                    trainable=False)) # set the weight to be not trainable (static)
+
+model.add(Bidirectional(LSTM(128, return_sequences=True))) 
+model.add(Dropout(0.2))
+model.add(Bidirectional(LSTM(256, return_sequences=True)))
+model.add(Dropout(0.2))
+model.add(Bidirectional(LSTM(128, return_sequences=False)))
+model.add(Dense(6, activation = 'softmax'))
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['sparse_categorical_accuracy'])
+
+config = model.get_config()
+
+history = model.fit(X_train_pad, y_train, 
+                    validation_data = (X_val_pad, y_validation),
+                    batch_size = 8,
+                    epochs = 3, 
+                    callbacks = stop)
+
+
+model.save_weights("model-weights")
+
+
+## print confusion matrix and accuracy, loss plots
+""" def plot_history(history):
+    '''
+    Plots training and validation accuracy and loss
+    Accepts a single param - history, where
+    history - keras.callbacks.History object
+    Returns nothing
+    
+    '''
+    loss = history.history['loss']
+    accuracy = history.history['accuracy']
+    val_loss = history.history['val_loss']
+    val_accuracy = history.history['val_accuracy']
+    x = range(1, len(loss) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, accuracy, label='Training acc', color='#707bfb')
+    plt.plot(x, val_accuracy, label='Validation acc', color='#fbcbff')
+    plt.title('Training and validation accuracy')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, label='Training loss', color='#707bfb')
+    plt.plot(x, val_loss, label='Validation loss', color='#fbcbff')
+    plt.title('Training and validation loss')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+plot_history(history)
+
+model.evaluate(X_test_pad, y_test) 
+
+y_pred = np.argmax(model.predict(X_test_pad), axis=1)
+
+from sklearn import metrics
+print(metrics.classification_report(y_test, y_pred))
+
+from matplotlib.colors import LinearSegmentedColormap
+colors = ['#ffffff', '#fbcbff', '#707bfb']
+cmap = LinearSegmentedColormap.from_list('mycmap', colors)
+
+def plot_confusion_matrix(matrix, fmt=''):
+  '''
+  Accepts a confusion matrix and a format param
+  Plots the matrix as a heatmap
+  Returns nothing
+
+  '''
+  plt.figure(figsize=(6, 5))
+  sns.heatmap(matrix, annot=True, 
+              cmap=cmap, 
+              fmt=fmt, 
+              xticklabels=emotions_to_labels.keys(), 
+              yticklabels=emotions_to_labels.keys())
+  plt.ylabel('True labels')
+  plt.xlabel('Predicted labels')
+  plt.show()
+
+matrix = metrics.confusion_matrix(y_test, y_pred)
+plot_confusion_matrix(matrix)
+
+matrix_new = np.zeros(matrix.shape)
+
+for row in range(len(matrix)):
+  sum = np.sum(matrix[row])
+  for element in range(len(matrix[row])):
+    matrix_new[row][element] = matrix[row][element] / sum
+
+plot_confusion_matrix(matrix_new, fmt='.2') """
